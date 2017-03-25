573e413ab701b498fd18ec8a9e40442f920b3c60
ErazerBrecht <erazerbrecht@gmail.com>
2017-03-25 10:33:07 +0100

    Added some AuthenticationManager UnitTests

--- test/BLL.Test/AuthenticationManagerUnitTests.cs
+++ test/BLL.Test/AuthenticationManagerUnitTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Authentication;
 using System.Threading.Tasks;
 using BLL.Authentication;
 using BLL.Authentication.Interfaces;
@@ -23,7 +24,10 @@ namespace BLL.Test
     {
         private const string Audience = "TestAudience";
         private const string Issuer = "TestIssuer";
+        private const string TestPassword = "@Passw0rd";
+        private readonly User _testUser;
         private readonly IAuthenticationManager _manager;
+        private readonly UserManager<User> _userManager;
 
         public AuthenticationManagerUnitTests()
         {
@@ -40,11 +44,13 @@ namespace BLL.Test
 
             var serviceProvider = services.BuildServiceProvider();
 
-            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            _userManager = serviceProvider.GetRequiredService<UserManager<User>>();
             var signinManager = serviceProvider.GetRequiredService<SignInManager<User>>();
-            userManager.CreateAsync(new User { UserName = "ErazerBrecht", FirstName = "Erazer", LastName = "Brecht" }).Wait();
+            _testUser = new User {UserName = "ErazerBrecht", FirstName = "Erazer", LastName = "Brecht"};
+            _userManager.CreateAsync(_testUser).Wait();
+            _userManager.AddPasswordAsync(_testUser, TestPassword).Wait();
 
-            _manager = new AuthenticationManager(userManager, signinManager, StubJwtOptions(), new LoggerFactory());
+            _manager = new AuthenticationManager(_userManager, signinManager, StubJwtOptions(), new LoggerFactory());
         }
 
         [TestMethod]
@@ -146,7 +152,23 @@ namespace BLL.Test
             // Arrange
             var credentials = new RegisterViewModel
             {
-                Login = "ErazerBrecht",
+                Login = _testUser.UserName,
+                FirstName = _testUser.FirstName,
+                LastName = _testUser.LastName,
+                Password = TestPassword
+            };
+
+            // Act
+            await _manager.RegisterUser(credentials);
+        }
+
+        [TestMethod]
+        public async Task RegisterValidUserShouldSucceed()
+        {
+            // Arrange
+            var credentials = new RegisterViewModel
+            {
+                Login = "ErazerBrecht2",
                 FirstName = "Erazer",
                 LastName = "Brecht",
                 Password = "@Passw0rd"
@@ -154,6 +176,110 @@ namespace BLL.Test
 
             // Act
             await _manager.RegisterUser(credentials);
+            var createdUser = await _userManager.FindByNameAsync(credentials.Login);
+            var password = await _userManager.CheckPasswordAsync(createdUser, credentials.Password);
+
+            // Assert
+            Assert.IsNotNull(createdUser);
+            Assert.AreEqual(credentials.FirstName, createdUser.FirstName); 
+            Assert.AreEqual(credentials.LastName, createdUser.LastName);
+            Assert.AreEqual(DateTime.UtcNow.Date, createdUser.Created.Date);
+            Assert.AreEqual(DateTime.UtcNow.Hour, createdUser.Created.Hour);
+            Assert.AreEqual(DateTime.UtcNow.Minute, createdUser.Created.Minute);
+            Assert.IsTrue(password);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LoginWithoutCredentialsShouldThrowException()
+        {
+            await _manager.LoginUser(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LoginWithoutLoginShouldThrowException()
+        {
+            // Arrange
+            var credentials = new UserCredentialsViewModel()
+            {
+                Login = null,
+                Password = "@Passw0rd"
+            };
+
+            // Act
+            await _manager.LoginUser(credentials);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task LoginWithoutPasswordShouldThrowException()
+        {
+            // Arrange
+            var credentials = new UserCredentialsViewModel
+            {
+                Login = "ErazerBrecht",
+                Password = null
+            };
+
+            // Act
+            await _manager.LoginUser(credentials);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCredentialException))]
+        public async Task LoginWithNonExistingUserShouldThrowException()
+        {
+            // Arrange
+            var user = new UserCredentialsViewModel()
+            {
+                Login = "ErazerBrecht3",
+                Password = "@Passw0rd"
+            };
+
+            // Act
+           await  _manager.LoginUser(user);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(AuthenticationException))]
+        public async Task LoginWithWrongPasswordShouldThrowException()
+        {
+            // Arrange
+            var user = new UserCredentialsViewModel
+            {
+                Login = _testUser.UserName,
+                Password = TestPassword + "UNVALID"
+            };
+
+            // Act
+            await _manager.LoginUser(user);
+        }
+
+        [TestMethod]
+        public async Task LoginWithCorrectCredentialsShouldReturnToken()
+        {
+            // Arrange
+            var user = new UserCredentialsViewModel()
+            {
+                Login = _testUser.UserName,
+                Password = TestPassword
+            };
+
+            // Act
+            var result = await _manager.LoginUser(user);
+            var savedUser = await _userManager.FindByNameAsync(user.Login);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(AuthenticationResultViewModel));
+            Assert.AreEqual(_testUser.UserName, result.Login);
+            Assert.AreEqual(_testUser.FirstName, result.FirstName);
+            Assert.AreEqual(_testUser.LastName, result.LastName);
+            Assert.IsNotNull(savedUser);
+            Assert.AreEqual(DateTime.UtcNow.Date, savedUser.LastLogin.Date);
+            Assert.AreEqual(DateTime.UtcNow.Hour, savedUser.LastLogin.Hour);
+            Assert.AreEqual(DateTime.UtcNow.Minute, savedUser.LastLogin.Minute);
+            Assert.IsNotNull(result.Token);
         }
 
         #region Helpers
